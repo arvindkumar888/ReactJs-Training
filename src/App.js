9181225fa04dc18ce821af8908599daa6cc0f089
arvindkumar888@gmail.com <arvindkumar888@gmail.com>
2019-04-29 12:18:49 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Header} from './components/Header'
+import {Footer} from './components/Footer'
+import {Login} from './components/Login'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header >
+        <img src={logo} width="150" height="150" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +21,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <Header title="React SPA Project"/>
+      <Login portal="Verizon"/>
+      <h1>Put your components here</h1>
+      <Footer/>
     </div>
   );
 }
