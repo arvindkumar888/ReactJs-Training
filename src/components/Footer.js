6b8b8e08dff7e1bdbadbba1fadfbca236ee33dfe
arvindkumar888@gmail.com <arvindkumar888@gmail.com>
2019-04-29 11:58:49 +0530

    2nd

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export const Footer =()=> {
+    return (
+        <div>
+            <h4 className="bg-warning text-center">
+            Copyright zReserved to Arvind
+            </h4>
+        </div>
+    )
+}
