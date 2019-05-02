4ad9b40efb7852b26a8e7e2b21be98602cb3a6c5
arvindkumar888@gmail.com <arvindkumar888@gmail.com>
2019-05-02 12:14:20 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -8,13 +8,10 @@ import Dashboard from './components/Dashboard'
 function App() {
   return (
     <div className="App">
-      <header>
-        <img src={logo} width="150" height="150" className="App-logo" alt="logo" />
+      <header className="App-header">
+        <img src={logo} width="150" height="50" className="App-logo" alt="logo" />
       </header>
-      <Header title="React SPA Project"/>
       <Dashboard />
-      <Login portal="Verizon"/>
-      <Footer year="@2019"/>
     </div>
   );
 }
